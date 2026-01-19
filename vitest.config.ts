dfbea9c779ff0e872ae3ceb2041536c59bb0fa20
BRYCENVN\tt_tuan <tt_tuan@brycen.com.vn>
2026-01-19 13:28:21 +0900

    no message

---
+++ vitest.config.ts
@@ -0,0 +1,26 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    coverage: {
+      reporter: ['text', 'json', 'html', 'lcov'],
+      exclude: [
+        'node_modules/',
+        'src/test/',
+        '**/*.d.ts',
+        'cypress/',
+        'cypress.config.ts',
+        '**/vitest.config.ts',
+        '**/*.config.js',
+        '**/*.config.ts'
+      ]
+    }
+  }
+})
